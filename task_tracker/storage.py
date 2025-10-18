3edf6b32a09534baddc67fa9e43b329c14981345
SMTK <smtktom@gmail.com>
2025-10-18 23:09:41 +0200

    creating save_data, load_data funcs for storing tasks

--- task_tracker/storage.py
+++ task_tracker/storage.py
@@ -1,1 +1,31 @@
-# čtení/zápis dat (např. JSON soubor)
+import json
+import datetime
+from pathlib import Path
+from models import Task
+
+
+DATA = Path("data") / "tasks.json"
+
+def save_data(path, tasks):
+    """Saving tasks to JSON file."""
+    
+    with open(path, "w") as f:
+        data = [task.__dict__ for task in tasks]
+        json.dump(data, f, default=str, indent=4)
+        
+        
+def load_data(path):
+    """Loading data from JSON file."""
+    
+    try:
+        with open(path, "r") as f:
+            items = json.load(f)
+            data_tasks = [Task(id=item["id"], description=item["description"], status=item["status"]) for item in items]
+            for task, item in zip(data_tasks, items):
+                task.created_at = datetime.fromisoformat(item["created_at"])
+                task.updated_at = datetime.fromisoformat(item["updated_at"])
+            return data_tasks
+    
+    except FileNotFoundError:
+        print("File does not exist. Returning empty task list.")
+        return []
