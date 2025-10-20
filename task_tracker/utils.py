665db7182468f0509b0998e41e7967800f2c4c61
SMTK <smtktom@gmail.com>
2025-10-20 14:26:47 +0200

    created MENU message

--- task_tracker/utils.py
+++ task_tracker/utils.py
@@ -1,1 +1,8 @@
-# pomocné funkce (validace, formátování apod.)
+menu = """
+=======MENU======
+1. My tasks - 'show'
+2. Add task - 'add Your Task'
+3. Update task - 'update ID Your new task'
+4. Delete task - 'delete ID'
+5. Exit program - 'exit'
+"""
