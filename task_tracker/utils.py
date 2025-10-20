496932f58d5be3ce3e0de76ee6d65083483abf44
SMTK <smtktom@gmail.com>
2025-10-20 21:21:54 +0200

    parser (argparse) moved to utils.py

--- task_tracker/utils.py
+++ task_tracker/utils.py
@@ -1,3 +1,5 @@
+import argparse
+
 menu = """
 =======MENU======
 1. My tasks - 'show'
@@ -6,3 +8,12 @@ menu = """
 4. Delete task - 'delete ID'
 5. Exit program - 'exit'
 """
+
+
+def get_parser():
+    # Argument Parser - commands for cli interaction
+    parser = argparse.ArgumentParser(description="Task Tracker CLI app")
+    parser.add_argument("action", choices=["add", "update", "delete", "done","progress", "list-all", "list-done"], help="Akce s úkoly")
+    parser.add_argument("id", nargs="?", help="ID úkolu")
+    parser.add_argument("value", nargs="?", help="Popis úkolu nebo nový úkol")
+    return parser
