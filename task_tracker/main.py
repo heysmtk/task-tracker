841446c8912ad45a8c846d31334995ef4cc962c9
SMTK <smtktom@gmail.com>
2025-10-20 21:55:43 +0200

    little changes

--- task_tracker/main.py
+++ task_tracker/main.py
@@ -1,19 +1,12 @@
-import argparse
 from models import TaskManager
-from utils import menu
+from utils import menu, get_parser
 
 
 # Task Manager - manager (creating instance)
 manager = TaskManager()
 
-# Argument Parser - commands for cli interaction
-parser = argparse.ArgumentParser(description="Task Tracker CLI app")
-parser.add_argument("action", choices=["add", "update", "delete", "done","progress", "list-all", "list-done"], help="Akce s úkoly")
-parser.add_argument("id", nargs="?", help="ID úkolu")
-parser.add_argument("value", nargs="?", help="Popis úkolu nebo nový úkol")
-
 # Load arguments from command line
-args = parser.parse_args()
+args = get_parser().parse_args()
 
 
 # App logic
@@ -21,19 +14,25 @@ if args.action == "add":
     description = args.value or args.id
     manager.add_task(description)
     print(f"Task was added: {description}")
+    
 elif args.action == "update":
     manager.update_task(int(args.id), args.value)
     print(f"Task (ID:{args.id}) was changed: {args.value}")
+    
 elif args.action == "delete":
     manager.delete_task(int(args.id))
     print(f"Task (ID:{args.id}) was deleted.")
+    
 elif args.action == "done":
     manager.mark_task_done(int(args.id))
     print(f"Task (ID:{args.id}) is marked as done.")
+    
 elif args.action == "progress":
     manager.mark_task_in_progress(int(args.id))
     print(f"Task (ID:{args.id}) is marked as in progress.")
+    
 elif args.action == "list-all":
     manager.list_all_tasks()
+    
 elif args.action == "list-done":
     manager.list_done_tasks()
